da2a34abd96b31fa7ed2132583ec50e23fdf5d6b
Brahian Quintero <brahianquintero07@gmail.com>
2025-04-24 09:36:15 -0500

    Creación de proyecto, capa de modelo y persistencia
    modelo con jpa y persistencia

---
+++ TallerJPA/src/main/java/co/edu/sena/persa/model/Career.java
@@ -0,0 +1,119 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.sena.persa.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Aprendiz
+ */
+@Entity
+@Table(name = "career")
+@NamedQueries({
+    @NamedQuery(name = "Career.findAll", query = "SELECT c FROM Career c"),
+    @NamedQuery(name = "Career.findById", query = "SELECT c FROM Career c WHERE c.id = :id"),
+    @NamedQuery(name = "Career.findByName", query = "SELECT c FROM Career c WHERE c.name = :name"),
+    @NamedQuery(name = "Career.findByType", query = "SELECT c FROM Career c WHERE c.type = :type")})
+public class Career implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Long id;
+    @Basic(optional = false)
+    @Column(name = "name")
+    private String name;
+    @Basic(optional = false)
+    @Column(name = "type")
+    private String type;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "careerId")
+    private Collection<Course> courseCollection;
+
+    public Career() {
+    }
+
+    public Career(Long id) {
+        this.id = id;
+    }
+
+    public Career(Long id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Collection<Course> getCourseCollection() {
+        return courseCollection;
+    }
+
+    public void setCourseCollection(Collection<Course> courseCollection) {
+        this.courseCollection = courseCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Career)) {
+            return false;
+        }
+        Career other = (Career) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "co.edu.sena.persa.model.Career[ id=" + id + " ]";
+    }
+    
+}
---
+++ TallerJPA/src/main/java/co/edu/sena/persa/model/Users.java
@@ -0,0 +1,189 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.sena.persa.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Aprendiz
+ */
+@Entity
+@Table(name = "users")
+@NamedQueries({
+    @NamedQuery(name = "Users.findAll", query = "SELECT u FROM Users u"),
+    @NamedQuery(name = "Users.findById", query = "SELECT u FROM Users u WHERE u.id = :id"),
+    @NamedQuery(name = "Users.findByFullname", query = "SELECT u FROM Users u WHERE u.fullname = :fullname"),
+    @NamedQuery(name = "Users.findByEmail", query = "SELECT u FROM Users u WHERE u.email = :email"),
+    @NamedQuery(name = "Users.findByPassword", query = "SELECT u FROM Users u WHERE u.password = :password"),
+    @NamedQuery(name = "Users.findByStatus", query = "SELECT u FROM Users u WHERE u.status = :status")})
+public class Users implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Long id;
+    @Basic(optional = false)
+    @Column(name = "fullname")
+    private String fullname;
+    @Basic(optional = false)
+    @Column(name = "email")
+    private String email;
+    @Basic(optional = false)
+    @Column(name = "password")
+    private String password;
+    @Basic(optional = false)
+    @Column(name = "status")
+    private String status;
+    @ManyToMany(mappedBy = "usersCollection")
+    private Collection<Course> courseCollection;
+    @ManyToMany(mappedBy = "usersCollection1")
+    private Collection<Course> courseCollection1;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "instructorId")
+    private Collection<Permission> permissionCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "guardId")
+    private Collection<Permission> permissionCollection1;
+    @JoinColumn(name = "role_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Roles roleId;
+
+    public Users() {
+    }
+
+    public Users(Long id) {
+        this.id = id;
+    }
+
+    public Users(Long id, String fullname, String email, String password, String status) {
+        this.id = id;
+        this.fullname = fullname;
+        this.email = email;
+        this.password = password;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Collection<Course> getCourseCollection() {
+        return courseCollection;
+    }
+
+    public void setCourseCollection(Collection<Course> courseCollection) {
+        this.courseCollection = courseCollection;
+    }
+
+    public Collection<Course> getCourseCollection1() {
+        return courseCollection1;
+    }
+
+    public void setCourseCollection1(Collection<Course> courseCollection1) {
+        this.courseCollection1 = courseCollection1;
+    }
+
+    public Collection<Permission> getPermissionCollection() {
+        return permissionCollection;
+    }
+
+    public void setPermissionCollection(Collection<Permission> permissionCollection) {
+        this.permissionCollection = permissionCollection;
+    }
+
+    public Collection<Permission> getPermissionCollection1() {
+        return permissionCollection1;
+    }
+
+    public void setPermissionCollection1(Collection<Permission> permissionCollection1) {
+        this.permissionCollection1 = permissionCollection1;
+    }
+
+    public Roles getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Roles roleId) {
+        this.roleId = roleId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Users)) {
+            return false;
+        }
+        Users other = (Users) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "co.edu.sena.persa.model.Users[ id=" + id + " ]";
+    }
+    
+}
